87c63322aa2587a54fd3ca8b298f79a4f9e3de8d
Your Name <you@example.com>
2021-04-03 18:02:22 -0300

    terminado

--- script.js
+++ script.js
@@ -10,29 +10,70 @@ const classNames = {
   const itemCountSpan = document.getElementById('item-count')
   const uncheckedCountSpan = document.getElementById('unchecked-count')
   
-  let contador = 0;
-  let cont = 0;
+  let tarea = '';
 
   function addTodo() {
     
-    const nuevoTitulo = prompt("Que tarea deseas agregar?")
-    if(nuevoTitulo != undefined){
-      contador++
-      itemCountSpan.innerHTML = contador
+    tarea = prompt("Que tarea deseas agregar?")
+    if(tarea !==  null && tarea.trim().length > 0){
       
-      addElement (nuevoTitulo)
+      let elemento = crearInput(tarea);
+      let nuevoLi = crearLi(tarea);
+
+      nuevoLi.prepend(elemento);
+      addElement (nuevoLi);
+
+      actualizaContadores();
     }
 
   }
 
   function addElement (titulo) {
-    let newli = document.createElement("li");
-    let input = document.createElement("INPUT");
-    input.type = 'checkbox'
-    input.checked = true
-    let contenido = document.createTextNode(titulo)
-    newli.appendChild(input)
-    newli.appendChild(contenido)
-    list.appendChild(newli)
-    
+    list.appendChild(titulo);
+  }
+
+  function crearInput(name){
+    let newInput = document.createElement('INPUT');
+    newInput.setAttribute("type","checkbox");
+    newInput.setAttribute("value","valor_checkbox");
+    newInput.setAttribute("id",name);
+    newInput.className += classNames.TODO_CHECKBOX;
+    newInput.setAttribute("onclick","actualizaContadores()");
+    newInput.checked = true;
+    return newInput;
+  }
+
+  function crearLi(texto){
+    let li = document.createElement("li");
+    li.innerText = texto;
+    return li;
+  }
+
+  function actualizaContadores(){
+    updateItemCount()
+    updateUncheckedCount()
+  }
+  
+  function updateItemCount(){
+    let items = document.getElementsByClassName(classNames.TODO_CHECKBOX);
+    let contador = 0;
+
+    for(let i = 0; i < items.length; i++){
+      if(items[i].checked){
+        contador++
+      }
+    }
+    itemCountSpan.innerText = contador;
+  }
+
+  function updateUncheckedCount(){
+    let items = document.getElementsByClassName(classNames.TODO_CHECKBOX);
+    let contador = 0;
+
+    for(let i = 0; i < items.length; i++){
+      if(!items[i].checked){
+        contador++
+      }
+    }
+    uncheckedCountSpan.innerText = contador;
   }
