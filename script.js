b4c73b4d66524ac8f1570e87be54b21365377afb
Your Name <you@example.com>
2021-04-03 00:51:18 -0300

    por ahora agrega la lista y el checked

--- script.js
+++ script.js
@@ -10,6 +10,29 @@ const classNames = {
   const itemCountSpan = document.getElementById('item-count')
   const uncheckedCountSpan = document.getElementById('unchecked-count')
   
+  let contador = 0;
+  let cont = 0;
+
   function addTodo() {
-    alert('Boton Add TODO clickeado!')
+    
+    const nuevoTitulo = prompt("Que tarea deseas agregar?")
+    if(nuevoTitulo != undefined){
+      contador++
+      itemCountSpan.innerHTML = contador
+      
+      addElement (nuevoTitulo)
+    }
+
+  }
+
+  function addElement (titulo) {
+    let newli = document.createElement("li");
+    let input = document.createElement("INPUT");
+    input.type = 'checkbox'
+    input.checked = true
+    let contenido = document.createTextNode(titulo)
+    newli.appendChild(input)
+    newli.appendChild(contenido)
+    list.appendChild(newli)
+    
   }
